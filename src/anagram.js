9ef891d4982cf74ab1992792de68b0c30b0cf861
2405-011-pw <walypa56@gmail.com>
2024-06-21 10:12:11 +0100

    working code

--- src/anagram.js
+++ src/anagram.js
@@ -4,7 +4,37 @@
  * @returns {boolean} the sum of a and b
  */
 function isAnagram(string1, string2) {
-    return false;
+    const occurences = {}
+    if (!(string1.length === string2.length)) {
+        return false
+    }
+
+    string1 = string1.toLowerCase();
+    string2 = string2.toLowerCase();
+
+    for (let letter of string1) {
+        if (!occurences.hasOwnProperty(letter)) {
+            occurences[letter] = 1
+        } else {
+            occurences[letter] += 1
+        }
+    }
+
+    for (let letter of string2) {
+        if (!(letter in occurences)) {
+            return false
+        } else {
+            occurences[letter] -= 1
+        }
+    }
+
+    for (let letter in occurences) {
+        if (occurences[letter] !== 0 ) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 export { isAnagram };
