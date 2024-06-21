b845cd71c47f7e91b332f205c2568899f8fbc64a
2405-011-pw <walypa56@gmail.com>
2024-06-21 10:48:07 +0100

    stuff

--- src/anagram.js
+++ src/anagram.js
@@ -37,4 +37,31 @@ function isAnagram(string1, string2) {
   return true;
 }
 
-export { isAnagram };
+function countOccurences(inputString) {
+    const results = {}
+    for (let i of inputString) {
+      let letter = i.toLowerCase(); //case insensitive
+      if (letter in results) {
+        results[letter]++;
+      } else {
+        results[letter] = 1;
+      }
+    }
+    return results;
+  }
+//compares objects like: {h: 1, e: 2},{h:1, e: 2}
+function compareObject(obj1, obj2) {
+    let keys1 = Object.keys(obj1);
+    let keys2 = Object.keys(obj2);
+    if (keys1.length !== keys2.length) return false;
+
+    for (let key of keys1) {
+        if (!keys2.includes(key) || !compareObject(obj1[key], obj2[key])) {
+            return false;
+        }
+    }
+
+    return true
+}
+
+export { isAnagram, countOccurences, compareObject };
--- src/anagram.test.js
+++ src/anagram.test.js
@@ -1,4 +1,4 @@
-import { isAnagram } from "./anagram.js";
+import { isAnagram, compareObject } from "./anagram.js";
 import { test, expect } from "@jest/globals";
 
 // jest docs for expect "matchers"
@@ -13,4 +13,10 @@ test("check for case insesitive anagrams", () => {
   expect(isAnagram('peach', 'cheap')).toBe(true);
   expect(isAnagram('knight', 'night')).toBe(false);
   expect(isAnagram('bbbhellobbb','aaahelloaaa')).toBe(false)
+  expect(isAnagram('abb','baa')).toBe(false)
 });
+
+test("Compare 2 objects", () => {
+  expect(compareObject({h: 1, e: 2},{h:1, e: 2})).toBe(true)
+  expect(compareObject({h: 1, e: 2},{h:1, e: 1})).toBe(false)
+})
